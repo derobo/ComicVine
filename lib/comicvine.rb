b6f856c3da68fe82838b952315a1b25220ab5317
Robert Mrasek <robo@derobo.de>
2011-10-15 12:09:57 +0200

    updated readme

--- lib/comicvine.rb
+++ lib/comicvine.rb
@@ -11,13 +11,3 @@ require 'comicvine/issue'
 require 'comicvine/character'
 #require 'comicvine/story_arc'
 require 'comicvine/volume'
-
-
-ComicVine::Connector.api_key = "0da061bf2dd352f6db1ec0229e665d3031a70c5d"
-@s = ComicVine::Search.new
-@s << ComicVine::Volume
-@r = @s.query("iron")
-@v = ComicVine::Volume.load 2407
-@c = ComicVine::Character.load 59175
-debugger
-puts @c
